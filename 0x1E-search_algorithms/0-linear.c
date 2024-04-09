df193fc9fc6aea713d37318c5f9071a7c7fcd2ab
Nanakzy <nanakzy@gmail.com>
2024-04-09 13:46:57 -0700

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
-	
+
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
@@ -21,5 +21,5 @@ int linear_search(int *array, size_t size, int value)
 		if (array[i] == value)
 			return (i);
 	}
-	return (-1);
+	return (-1)
 }
